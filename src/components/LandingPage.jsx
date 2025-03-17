8642d239a78e2affaca60f74ce67a563f900eb44
Aditya Singh <singhaditya00005@outlook.com>
2025-03-17 23:43:15 +0530

    local changes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import Navbar from './Navbar'
 import SplineScene from './SplineScene'
+import GridMotion from './GridMotion';
 
 
 const LandingPage = () => {
+
+  const items = [
+    'Item 1',
+    <div key='jsx-item-1'>Custom JSX Content</div>,
+    'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'Item 2',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'Item 4',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'Item 5',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'Item 7',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'Item 8',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'Item 10',
+    <div key='jsx-item-3'>Custom JSX Content</div>,
+    'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'Item 11',
+    <div key='jsx-item-2'>Custom JSX Content</div>,
+    'Item 13',
+    <div key='jsx-item-4'>Custom JSX Content</div>,
+    'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'Item 14',
+    // Add more items as needed
+  ];
+
   return (
     <div>
       <Navbar />
       <SplineScene />
-      <div className="h-screen w-screen bg-gradient-to-t from-[rgba(244,208,136,255)] via-[rgba(201,184,156,255)] to-[rgba(146,141,130,255)]"></div>
+      <div className="h-screen w-screen bg-gradient-to-t from-[rgba(244,208,136,255)] via-[rgba(201,184,156,255)] to-[rgba(146,141,130,255)]">
+        <GridMotion className=""/>
+      </div>
       
 
     </div>
